a08c10873497e98ffbc7fa662836db0c19afd389
Mariana <marianaplazas@live.com>
2019-08-17 04:58:28 +0000

    more corrections in to advance

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,18 +1,20 @@
-# puppet adavance task
+#puppet advance
 exec { 'update':
-  command  => 'sudo apt-get update',
-  provider => shell,
+command => 'usr/bin/apt-get update'
 }
+
 -> package{'nginx':
   ensure => present,
 }
--> file_line {'header':
-  path => 'etc/nginx/conf/nginx.conf',
-  match => 'http {',	
-  line => "http {
-     	add_header X-Served_By "${hostname}";",
+-> file_line {'line':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "	location / {
+  	 add_header X-Served-By ${hostname};",
+  match  => '^\tlocation / {',
 }
--> exec { 'restart':
+
+-> exec { 'restart nginx':
   command  => 'sudo service nginx restart',
   provider => shell,
 }
