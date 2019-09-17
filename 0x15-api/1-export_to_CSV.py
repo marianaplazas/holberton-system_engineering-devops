997296e7f83943dda34f5d0a7c0b292b1ced4b67
Mariana <marianaplazas@live.com>
2019-09-17 01:22:28 +0000

    [add] first version of the task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+ Python Script and export json to CSV
+"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    usr_id = sys.argv[1]
+    todo_list = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(
+        usr_id)).json()
+    username = user.get("username")
+    with open('{}.csv'.format(usr_id), 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ',
+                                quotechar='"', quoting=csv.QUOTE_ALL)
+        for task in todo_list:
+            if task.get("userId") == int(usr_id):
+                spamwriter.writerow([usr_id, username, task.get("completed"),
+                                     task.get("title")])
