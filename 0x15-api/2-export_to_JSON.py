d2209eeaea423f31eae2c77004c73af61cb5feb4
Mariana <marianaplazas@live.com>
2019-09-17 01:58:54 +0000

    [fix] output 2 point

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-convert to JSON format
+JSON format
 """
 import json
 import requests
@@ -8,13 +8,13 @@ import sys
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com"
-    users = requests.get(url + "/users", params={"id": sys.argv[1]})
+    root = "https://jsonplaceholder.typicode.com"
+    users = requests.get(root + "/users", params={"id": sys.argv[1]})
     for names in users.json():
         usr_id = names.get('id')
-        todo_list = requests.get(url + "/todos", params={"userId": usr_id})
+        todo = requests.get(root + "/todos", params={"userId": usr_id})
         csv_arr = []
-        for tasks in todo_list.json():
+        for tasks in todo.json():
             csv_arr.append({"task": tasks.get("title"),
                             "completed": str(tasks.get("completed")),
                             "username": names.get("name")})
