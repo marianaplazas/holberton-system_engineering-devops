15056265ff2211e10e1b540ec3ad31d0438a8adc
Mariana <marianaplazas@live.com>
2019-09-17 00:53:07 +0000

    [fix] the bad syntax

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,22 +2,22 @@
 """
 Gets the completed todo list of an employeed with the id
 """
-import requests
+from requests import get
 import sys
 
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
-    users = requests.get(url + "/users", params={"id": sys.argv[1]}).json()
+    users = get(url + "/users", params={"id": sys.argv[1]}).json()
     for names in users:
         user_id = names.get('id')
-        todo_list = requests.get(root + "/todos", params={"userId": user_id}).json()
+        todo_list = get(url + "/todos", params={"userId": user_id})
         task_complete = 0
         tasks= []
-        for task in todo_list():
+        for task in todo_list.json():
             if tasks.get('completed') is True:
                 task_complete += 1
                 tasks_array.append(tasks.get('title'))
         print("Employee {:s} is done with tasks({:d}/{:d}):\n\t {}".
               format(names.get('name'), task_complete,
-                     len(todo_list), "\n\t ".join(tasks)))
+                     len(todo_list.json()), "\n\t ".join(tasks)))
