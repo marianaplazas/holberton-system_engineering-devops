5692154a6570c73eb61657500a8f8fd5ca26a43c
Mariana <marianaplazas@live.com>
2019-09-17 02:02:51 +0000

    [fix] outpu

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
 """
-JSON format
+    Export to json file all tasks owned by an employee
 """
 import json
 import requests
-import sys
-
+from sys import argv
 
 if __name__ == "__main__":
-    root = "https://jsonplaceholder.typicode.com"
-    users = requests.get(root + "/users", params={"id": sys.argv[1]})
-    for names in users.json():
-        usr_id = names.get('id')
-        todo = requests.get(root + "/todos", params={"userId": usr_id})
-        csv_arr = []
-        for tasks in todo.json():
-            csv_arr.append({"task": tasks.get("title"),
-                            "completed": str(tasks.get("completed")),
-                            "username": names.get("name")})
-        with open(sys.argv[1] + ".json", 'a') as f:
-            f.write(json.dumps({"2": csv_arr}))
+    if len(argv) > 1:
+        user = argv[1]
+        api_url = "https://jsonplaceholder.typicode.com/"
+        req_user = requests.get("{}users/{}".format(api_url, user))
+        username = req_user.json().get("username")
+        all_tasks = requests.get(
+            "{}todos?userId={}".format(
+                api_url, user)).json()
+        task_list = []
+        user_dict = {}
+        for task in all_tasks:
+            task_dict = {}
+            task_dict["task"] = task.get("title")
+            task_dict["completed"] = task.get("completed")
+            task_dict["username"] = username
+            task_list.append(task_dict)
+        user_dict[user] = task_list
+        with open("{}.json".format(user), "w") as jsonfile:
+            json.dump(user_dict, jsonfile)
