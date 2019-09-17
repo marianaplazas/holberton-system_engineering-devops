d085b13856e2256a7c7553a9e24d4096af4e4f48
Mariana <marianaplazas@live.com>
2019-09-17 02:06:50 +0000

    [add] first version of the last point

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+create a dictionary list of dictionaries
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    root = "https://jsonplaceholder.typicode.com"
+    users = requests.get(root + "/users")
+    for names in users.json():
+        usr_id = names.get('id')
+        todo = requests.get(root + "/todos", params={"userId": usr_id})
+        csv_arr = []
+        for tasks in todo.json():
+            csv_arr.append({"task": tasks.get("title"),
+                            "completed": str(tasks.get("completed")),
+                            "username": names.get("name")})
+        with open("todo_all_employees.json", 'a') as f:
+            f.write(json.dumps({usr_id: csv_arr})
