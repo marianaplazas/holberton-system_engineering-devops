6635195dbbfc6380d466ccea3b6cd98a232d1387
Mariana <marianaplazas@live.com>
2019-09-19 21:40:27 +0000

    [fix] back to old version

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import requests
-from requests.exceptions import ConnectionError
 """
 recurse 
 """
@@ -8,20 +7,19 @@ recurse
 
 def recurse(subreddit, after=None, all_results=[]):
     'return the number of posts'
+     param = {}
+    if after is not None:
+        param = {'after': after}
     url = 'https://reddit.com/r/' + subreddit + '/hot/.json'
-    headers = {'User-Agent': 'My User Agent'}
-    req = requests.get(url, headers=headers, allow_redirects=False)
-    if (req.status_code == requests.codes.ok):
-        r = req.json()
-        data = r['data']['children']
-        for post in data:
-            hot_list.append(post['data']['title'])
-        after = r['data']['after']
-        if after is not None:
-            a = "?after={}".format(after)
-            recurse(subreddit, hot_list, a)
+    headers = {'User-Agent': "lala"}
+    r = requests.get(url, headers=headers, params=param)
+    try:
+        new_after = r.json()['data'].get('after')
+        for data in r.json()['data'].get('children'):
+            all_results.append(data['data'].get('title'))
+        if new_after is not None:
+            return(recurse(subreddit, new_after, all_results))
         else:
-            return hot_list
-    else:
-        return None
-    return hot_list
+            return(all_results)
+    except:
+        return(None)
